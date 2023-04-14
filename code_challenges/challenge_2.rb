d37d56e9bd7c39a02da3e2672747c44366415597
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-04-14 10:13:53 -0400

    edit ch2

--- code_challenges/challenge_2.rb
+++ code_challenges/challenge_2.rb
@@ -5,7 +5,7 @@ if good_driving_record == true && age >= 25
     puts "you get a discount"
 elsif good_driving_record == true || age >= 25  
     puts " pay full price"
-elsif good_driving_record == false && age < 25
+else 
     puts "you need someone else to sign"
  
 end
